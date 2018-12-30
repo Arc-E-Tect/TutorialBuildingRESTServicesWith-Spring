307378d2f1a7af6afdb87f40a6e6a5f433b89c11
Iwan Eising <iwan@i-ware.net>
2018-12-30 21:37:14 +0100

    Initial Upload

---
+++ src/main/java/payroll/LoadDatabase.java
@@ -0,0 +1,33 @@
+package payroll;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initEmployeeDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+        };
+    }
+
+    @Bean
+    CommandLineRunner initOrderDatabase(OrderRepository orderRepository) {
+        return args -> {
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
+        };
+    }
+
+}
